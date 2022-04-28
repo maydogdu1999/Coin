1a16f229249d204e22399dbdc667721176604dd2
Evan Albers <evanlucalbers@gmail.com>
2022-04-28 18:34:19 -0400

    encryption methods added

--- Node.java
+++ Node.java
@@ -4,6 +4,12 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.io.*;
 import java.util.concurrent.TimeUnit;
 
+import javax.crypto.Cipher;
+
+import java.security.*;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+
 public class Node extends Thread {
     static final int MAX_NEIGHBORS = 5;
     static final int IDEAL_NEIGHBORS = 3;
@@ -287,6 +293,109 @@ public class Node extends Thread {
         }
 
 
+    }
+
+    /**
+     * A method that converts a given string into a public key, necessary because key will be a string when read in from socket
+     * @param key String representing some public key
+     * @return A PublicKey object representing a public key
+     */
+    public PublicKey stringToPublicKey(String key) {
+
+        PublicKey pubKey = null; 
+
+        try {
+        byte[] pubKeyByte = Base64.getDecoder().decode(key);
+
+        PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(pubKeyByte);
+
+        KeyFactory keyFac = KeyFactory.getInstance("RSA");
+
+        pubKey = keyFac.generatePublic(keySpec);
+        
+        } catch (Exception e) {
+
+            System.out.println(e);
+
+        }
+
+        return pubKey;
+
+    }
+
+    /**
+     * This method encrypts a given string using the given private key. This is called "signing" a string. The resulting string can be decrypted using the corresponding
+     * private key.
+     * @param message A String to be encrypted
+     * @param privateKey a String representing the private key with which to encrypt
+     * @return a String representing the encrypted message
+     */
+    public String encryptMessage(String message, PrivateKey privateKey) {
+
+        Cipher encrypt = null;
+
+        String encryptedMessage = "";
+
+        try {
+
+            encrypt = Cipher.getInstance("RSA");
+
+            encrypt.init(Cipher.ENCRYPT_MODE, privateKey);
+
+            encryptedMessage = new String(encrypt.doFinal(Base64.getDecoder().decode(message)));
+
+        } catch (Exception e) {
+
+            System.out.println(e);
+
+        }
+
+        return encryptedMessage;
+    }
+
+    /**
+     * This method decrypts a given signature using the given public key. The resulting value is used by verifyTransaction to determine whether a message has been 
+     * properly signed (has been authorized by the given public key)
+     * @param signedMessage a String representing the "signature" in a transaction message
+     * @param publicKey a String representing the public key of the keypair attempting to make a transaction
+     * @return a String representing the hashed transaction message. 
+     */
+    public String decryptMessage(String signedMessage, String publicKey) {
+
+        String decryptedMessage = "";
+
+        PublicKey pubKey = stringToPublicKey(publicKey);
+
+        Cipher decrypt = null;
+
+        try {
+
+            decrypt = Cipher.getInstance("RSA");
+
+            decrypt.init(Cipher.DECRYPT_MODE, pubKey);
+
+            decryptedMessage = new String(decrypt.doFinal(Base64.getDecoder().decode(signedMessage)));
+
+        } catch (Exception e) {
+
+            System.out.println(e);
+
+        }
+
+        return decryptedMessage;
+    }
+
+    /**
+     * A method that determines whether the transaction has been properly signed. If so, it can be written to the blockchain. It takes the raw message recieved over 
+     * the socket, and outputs a boolean that is true if verification is successful, and false otherwise. 
+     * @param message A string that is the transaction message read in from socket.
+     * @return a boolean representing whether the transaction has been verified 
+     */
+    public Boolean verifyTransaction(String message) {
+
+
+
+        return true;
     }
 
 }
